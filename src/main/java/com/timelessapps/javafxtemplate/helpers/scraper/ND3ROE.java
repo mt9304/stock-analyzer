6e1b0bd8542976d4a488cdfdfcdde573804ec29d
MT9304 <MT9304@gmail.com>
2018-09-28 21:27:28 -0700

    Fixed index out of bounds for single decimal place roe.

--- src/main/java/com/timelessapps/javafxtemplate/helpers/scraper/ND3ROE.java
+++ src/main/java/com/timelessapps/javafxtemplate/helpers/scraper/ND3ROE.java
@@ -150,10 +150,11 @@ public class ND3ROE extends ND2EPS {
 				//Convert to only use decimal of last digit. 
 				if (rawROEValue.contains(".")) {
 					int decimalIndex = rawROEValue.indexOf(".");
-					//Sometimes result is 2.0. or 34325354.3 check if 2nd to last character is .
+					//Sometimes result is 2.0. or 34325354.3 check if 2nd to last character is ".". 
 					if (rawROEValue.split("")[rawROEValue.split("").length-2].equals(".")) {
 						rawROEValue = rawROEValue + "00";
 					}
+					
 					roeValue = rawROEValue.substring(0,decimalIndex+3);
 				} else {
 					roeValue = rawROEValue.substring(0,5);
@@ -239,10 +240,11 @@ public class ND3ROE extends ND2EPS {
 					//Convert to only use decimal of last 2 digits. 
 					if (rawROEValue.contains(".")) {
 						int decimalIndex = rawROEValue.indexOf(".");
-						//Sometimes result is 2.0
-						if (rawROEValue.length() < 4) {
+						//Sometimes result is 2.0. or 34325354.3 check if 2nd to last character is ".". 
+						if (rawROEValue.split("")[rawROEValue.split("").length-2].equals(".")) {
 							rawROEValue = rawROEValue + "00";
 						}
+						System.out.println("INDEX: " + rawROEValue);
 						roeValue = rawROEValue.substring(0,decimalIndex+3);
 					} else {
 						roeValue = rawROEValue.substring(0,5);
